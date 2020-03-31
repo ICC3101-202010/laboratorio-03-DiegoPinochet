98c82079de6b39919a5a028c088cfe58780921d2
Diego Pinochet <dppinochet@miuandes.cl>
2020-03-31 16:01:18 -0300

    creacion del programa

---
+++ Laboratorio3-Diego-Pincohet/Laboratorio3-Diego-Pincohet/Persona.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laboratorio3_Diego_Pincohet
+{
+    class Persona
+    {
+        private string Rut;
+        private string Nombre;
+        private string Apellido;
+        private string Fecha_de_nacimiento;
+        private string Nacionalidad;
+        private string Puesto_de_trabajo;
+
+        public Persona(string nombre,string apellido, string rut, string fecha_de_nacimiento, string nacionalidad, string puesto_de_trabajo) 
+        {
+            this.Nombre = nombre;
+            this.Apellido = apellido;
+            this.Rut = rut;
+            this.Fecha_de_nacimiento = fecha_de_nacimiento;
+            this.Nacionalidad = nacionalidad;
+            this.Puesto_de_trabajo = puesto_de_trabajo;
+        }
+
+        public string Cambiar_Trabajo()
+        {
+
+        }
+        public string Cambiar_Sueldo()
+        {
+
+        }
+        public string Cambiar_horario()
+        {
+
+        }
+    }
+}
---
+++ Laboratorio3-Diego-Pincohet/Laboratorio3-Diego-Pincohet/Producto.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laboratorio3_Diego_Pincohet
+{
+    class Producto
+    {
+        private string Precio;
+        private string Nombre;
+        private string Marca;
+        private string Stock_disponible;
+        private string Fecha_compra;
+
+        public Producto(string nombre, string precio, string marca, string stock_disponible, string fecha_compra)
+        {
+            this.Nombre = nombre;
+            this.Precio = precio;
+            this.Marca = marca;
+            this.Stock_disponible = stock_disponible;
+            this.Fecha_compra = fecha_compra;
+        }
+
+        
+    }
+}
